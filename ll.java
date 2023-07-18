02739bac181af4a54d373ad82196aa22bc6ac391
Deepika Chandrashekar Munimadagu <121370042+DeepikaMunimadagu@users.noreply.github.com>
2023-07-18 22:49:14 +0530

    Create ll.java

---
+++ ll.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Main {
+  public static class Node{
+    int data;
+    Node next;
+
+    public Node(int data){
+      this.data = data;
+      this.next = null;
+    }
+  }
+  public static Node head;
+  public static Node tail;
+
+  public void addFirst(int data){
+    //step-1 create new node
+    Node newNode = new Node(data);
+    if(head == null){
+      head = tail = newNode;
+      return;
+    }
+    //step-2 newNode next = head
+    newNode.next = head;
+    //step-3 head = newNode
+    head = newNode;
+      
+  }
+  public void addLast(int data){
+    Node newNode = new Node(data);
+    if(head == null){
+      head = tail = newNode;
+    }
+    tail.next = newNode;
+    tail = newNode;
+  }
+  public static void main(String args[]){
+    LinkedList ll = new LinkedList();
+    ll.addFirst(1);
+    ll.addFirst(2);
+    ll.addLast(3);
+    System.out.println("The node is : "+ll);
+  }
+}
