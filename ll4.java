cd56d28e4dc99f66d4602c85839f10a85607b4aa
Deepika Chandrashekar Munimadagu <121370042+DeepikaMunimadagu@users.noreply.github.com>
2023-07-24 22:08:05 +0530

    Update ll4.java

--- ll4.java
+++ ll4.java
@@ -37,7 +37,20 @@ public class New{
         }
         System.out.println("null");
     }
-    //remove
+    //reverse
+    public void reverse(){
+        Node curr = head;
+        Node prev = null;
+        Node next;
+        while(curr!=null){
+            next = curr.next;
+            curr.next = prev;
+            curr.prev = next;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
 
     public static void main(String args[]){
         New dll = new New();
